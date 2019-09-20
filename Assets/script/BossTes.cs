eb570cab0ecbdf7807cde10345e3345a5167891f
toshi_oba <toshizx.1114@gmail.com>
2019-09-20 09:58:23 +0900

    Lesson4の課題提出用です

---
+++ Assets/script/BossTes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class Boss1 {        
+		private int mp = 53;          // 魔力
+		
+	
+		// 魔法の関数
+	　　 public  void Magic() { 
+		if (mp >= 5) {
+			mp = mp - 5; 
+			Debug.Log( "魔法攻撃をした。残りmpは" + mp);
+		}else{
+			Debug.Log ("Mpが足りないため魔法が使えない");
+		}
+		}
+
+		
+		
+	　}
+
+　
+public class  BossTes: MonoBehaviour {
+		void Start () {
+			// Bossクラスの変数を宣言してインスタンスを代入
+			
+		  
+		Boss1 lastboss = new Boss1 ();  
+		for(int i = 0; i < 11; i++){
+			lastboss.Magic();
+		}
+
+
+			
+			//if(i<=0){
+				//Debug.Log("Mpが足りないため魔法が使えない");	
+			//lastboss.magic();
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+
---
+++ Assets/script/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = {10, 20, 30, 40, 50, };
+
+		// 配列の要素数のぶんだけ処理を繰り返す
+		for (int i = 0; i < array.Length; i++) {
+			
+				Debug.Log (array [i]);
+			}
+		for (int j = 4; 0 <= array.Length; j--) {
+
+		    	Debug.Log (array [j]);
+	
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
